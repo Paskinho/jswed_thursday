32a5bdfd5c44bfb912aae7a1f87c247c57a6f3a0
Paskanau Uladzislau <97022116+Paskinho@users.noreply.github.com>
2023-03-22 12:26:14 +0300

    Update 15-prototype.js

--- 15-prototype.js
+++ 15-prototype.js
@@ -1,1 +1,17 @@
+class User {
+    constructor(name) {
+        this.name = name
+        this.state = {}
+    }
 
+    render() {
+        return 'My name is ' + this.name
+    }
+}
+
+const user1 = new User ('dimych')
+const user2 = new User ('dimych')
+
+
+console.log(user1.__proto__ === User.prototype) //true
+console.log(user1.__proto__ === User.prototype) //true
